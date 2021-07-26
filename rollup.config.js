4bba7cba5674ca92d920d461c5af3163b1387406
Yadu S <yadu.s@ccs-technologies.com>
2021-07-26 13:38:54 +0530

    output plugins: here adding rollup-plugin-terser to minify output

--- rollup.config.js
+++ rollup.config.js
@@ -14,12 +14,26 @@ import json from '@rollup/plugin-json';
 // export default config;
 // rollup.config.js
 import { defineConfig } from 'rollup';
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
   input: 'src/main.js',
-  output: {
-    file: 'bundle.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'dist/bundle.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/bundle.min.js',
+      format: 'iife',
+      name: 'printVersion',
+      plugins: [terser()], // this does minification
+    },
+    {
+      file: 'dist/bundle.iife.js',
+      format: 'iife',
+      name: 'printVersionIife',
+    },
+  ],
   plugins: [json()],
 });
