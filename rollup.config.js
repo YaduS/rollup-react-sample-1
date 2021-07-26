bfc75383621ba92150bd86e8b7a9159c28205c08
Yadu S <yadu.s@ccs-technologies.com>
2021-07-26 13:15:46 +0530

    using plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,25 @@
-export default {
+import json from '@rollup/plugin-json';
+// we can use either jsDoc of defineConfig for type intellisense
+// /**
+//  * @type {import('rollup').RollupOptions}
+//  */
+// const config = {
+//   input: 'src/main.js',
+//   output: {
+//     file: 'bundle.js',
+//     format: 'cjs',
+//   },
+//   plugins: [json()],
+// };
+// export default config;
+// rollup.config.js
+import { defineConfig } from 'rollup';
+
+export default defineConfig({
   input: 'src/main.js',
   output: {
     file: 'bundle.js',
     format: 'cjs',
   },
-};
+  plugins: [json()],
+});
