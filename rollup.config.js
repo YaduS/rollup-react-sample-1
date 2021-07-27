2307f2adfcd4492397e38e27771b65922181f9a5
Yadu S <yadu.s@ccs-technologies.com>
2021-07-27 15:25:14 +0530

    using css.scss module.. this was difficult to figure out 😯
    using rollup-plugin-postcss now.. it supports scss and modules out of the box

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 import { defineConfig } from 'rollup';
 import del from 'rollup-plugin-delete';
-import scss from 'rollup-plugin-scss';
+// import scss from 'rollup-plugin-scss';
+import postcss from 'rollup-plugin-postcss';
 // import { terser } from 'rollup-plugin-terser';
 
 // defineConfig used for intellisense..
@@ -10,21 +11,26 @@ export default defineConfig({
   input: 'src/main.js',
   output: [
     {
-      dir: 'dist/js/cjs',
+      dir: 'dist/cjs',
       format: 'cjs',
       // plugins: [terser()],   // for minification
     },
     {
-      dir: 'dist/js/esm',
+      dir: 'dist/esm',
       format: 'esm',
       // plugins: [terser()],   // for minification
     },
   ],
   plugins: [
     json(),
-    scss({
-      output: 'dist/css/styles.css',
+    postcss({
+      // supports scss loaders out of the box so no need to specify anything..😯
+      autoModules: true,
+      extract: 'css/styles.css',
     }),
+    // scss({
+    //   output: 'dist/css/styles.css',
+    // }),
     babel({
       presets: ['@babel/preset-react'],
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
--- src/test.tsx
+++ src/test.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './test.module.scss';
 
 const test = () => {
-  return <div>This is a component from a tsx file..</div>;
+  return (
+    <div className={classes['test-class']}>
+      This is a component from a tsx file..
+    </div>
+  );
 };
 
 export default test;
