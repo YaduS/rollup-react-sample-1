dcaba38f75208617100bfbce698f946f73717222
Yadu S <yadu.s@ccs-technologies.com>
2021-07-27 10:17:34 +0530

    can trigger multiple entry points using the command: rollup src/main.js src/main2.js -f cjs -d dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,8 @@
 import json from '@rollup/plugin-json';
-// we can use either jsDoc of defineConfig for type intellisense
-// /**
-//  * @type {import('rollup').RollupOptions}
-//  */
-// const config = {
-//   input: 'src/main.js',
-//   output: {
-//     file: 'bundle.js',
-//     format: 'cjs',
-//   },
-//   plugins: [json()],
-// };
-// export default config;
-// rollup.config.js
 import { defineConfig } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
 
+// defineConfig used for intellisense..
 export default defineConfig({
   input: 'src/main.js',
   output: [
